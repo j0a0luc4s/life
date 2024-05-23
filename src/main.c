4e906d1e9f4e82e29708104143a3532680bce5bd
joao <joaolucasrrolim@gmail.com>
2024-05-23 08:25:44 -0300

    Raylib fullscreen toggle

--- src/main.c
+++ src/main.c
@@ -2,14 +2,29 @@
 
 int main(void)
 {
-    InitWindow(800, 450, "raylib [core] example - basic window");
+    SetConfigFlags(FLAG_WINDOW_RESIZABLE);
+    int monitor = GetCurrentMonitor();
+    InitWindow(GetMonitorWidth(monitor)/2, GetMonitorWidth(monitor)/2, "raylib [core] example - basic window");
 
     while (!WindowShouldClose())
     {
+        if (IsKeyPressed(KEY_F)) {
+            if (IsWindowFullscreen()) {
+                monitor = GetCurrentMonitor();
+                SetWindowSize(GetMonitorWidth(monitor)/2, GetMonitorWidth(monitor)/2);
+                ToggleFullscreen();
+            } else {
+                monitor = GetCurrentMonitor();
+                SetWindowSize(GetMonitorWidth(monitor), GetMonitorHeight(monitor));
+                ToggleFullscreen();
+            }
+        }
+
         BeginDrawing();
             ClearBackground(RAYWHITE);
             DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
         EndDrawing();
+
     }
 
     CloseWindow();
